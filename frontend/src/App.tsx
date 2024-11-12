c610573f13ac6ce399e84845bf19817abc56b235
Bharat <bharat.shah@mycomera.com>
2024-11-12 16:19:10 +0400

    adding front end codebase

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+// src/App.tsx
+import React, { Suspense, lazy } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+
+const Signin = lazy(() => import("./pages/auth/signin"));
+const Signup = lazy(() => import("./pages/auth/signup"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+}
+
+export default App;
