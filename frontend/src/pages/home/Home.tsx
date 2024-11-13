00f1593f65e110337ab8c34d471b337a47522eb0
Bharat <bharat.shah@mycomera.com>
2024-11-13 18:06:25 +0400

    adding single navigation

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
-import Template from 'common/Template/Template'
-import React from 'react'
+import Template from "common/Template/Template";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <Template message='Hello World'>
-        <></>
+    <Template message="Hello World">
+      <></>
+      <div className="navigation">
+        <Link to="/signin">Signin</Link>
+        <Link to="/signup">Signup</Link>
+      </div>
     </Template>
-  )
+  );
 }
